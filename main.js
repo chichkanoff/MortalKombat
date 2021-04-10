909f65f5355141e3132a17e02286d6c0c3f512a5
chichkanoff <chichkanoff@gmail.com>
2021-04-10 09:46:39 +0300

    HomeWork #3
    Было интересно. Не совсем очевидно откуда взята функция disabled у кнопки. В подсказках VS Code она не вышла.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-//Task #0
+const $arena = document.querySelector(".arenas");
+const $buttonRandom = document.querySelector('.button');
 
 const player1 = {
   name: "SCORPIO",
-  hp: 100,
+  player: 1, //Task #0
+  hp: 100, //Task #0
   img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   weapon: ["armo", "turbo"],
   attack: function () {
@@ -12,7 +14,8 @@ const player1 = {
 
 const player2 = {
   name: "SUB-ZERO",
-  hp: 50,
+  player: 2, //Task #0
+  hp: 100, //Task #0
   img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
   weapon: ["armo", "turbo"],
   attack: function () {
@@ -20,57 +23,63 @@ const player2 = {
   },
 };
 
-//Task #1
+function createElement(tagName,className) {
+  const $element = document.createElement(tagName);
+  if(className) {
+    $element.classList.add(className); 
+  }  
+  return $element;
+}
 
-function createPlayer(player, name, life) {
-  const $player = document.createElement("div");
-  $player.classList.add(player); //---parametr
-  const $progressbar = document.createElement("div");
-  $progressbar.classList.add("progressbar");
-  const $character = document.createElement("div");
-  $character.classList.add("character");
 
-  const $life = document.createElement("div");
-  $life.classList.add("life");
-  $life.style.width = life + "%"; //---parametr
-  const $name = document.createElement("div");
-  $name.classList.add("name");
-  $name.innerText = name; //---parametr
-  const $image = document.createElement("img");
-  $image.src = "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif";
-
-  $progressbar.appendChild($life);
-  $progressbar.appendChild($name);
-  $player.appendChild($progressbar);
-  $character.appendChild($image);
-  $player.appendChild($character);
-
-  const $arena = document.querySelector(".arenas");
-  $arena.appendChild($player);
+function changeHp(player) {
+  const $life = document.querySelector(".player" + player.player + " .life");
+  player.hp -= doDamage();//Task #2
+  if (player.hp < 0) player.hp = 0; //Task #1
+  $life.style.width = player.hp + "%";
+  console.log($life.style.width + " " + player.hp);
+  if (player.hp === 0) {
+    //$arena.appendChild(losePlayer(player.name));
+    //Task #3
+    $arena.appendChild(winPlayer(player.player === 1 ? player2.name : player1.name));
+    $buttonRandom.disabled = true;
+  }
 }
 
 //Task #2
+function doDamage () {
+  let strike = Math.ceil(Math.random() * 20);
+  console.log(strike);
+  return strike;
+}
 
-createPlayer("player1", "SCORPION", 100);
-createPlayer("player2", "SUB-ZERO", 50);
+function losePlayer(name) {
+  const $loseTitle = createElement('div', 'loseTitle');
+  $loseTitle.innerText = name + " lose";
+  return $loseTitle;
+}
 
 //Task #3
+function winPlayer(name) {
+  const $winTitle = createElement('div', 'loseTitle');
+  $winTitle.innerText = name + " wins";
+  return $winTitle;
+}
 
-function createPlayerStar(player, obj) {
-  const $player = document.createElement("div");
-  $player.classList.add(player);
-  const $progressbar = document.createElement("div");
-  $progressbar.classList.add("progressbar");
-  const $character = document.createElement("div");
-  $character.classList.add("character");
+$buttonRandom.addEventListener('click',function() {
+  changeHp(player1);
+  changeHp(player2);
+})
 
-  const $life = document.createElement("div");
-  $life.classList.add("life");
+function createPlayer(obj) {
+  const $player = createElement("div", "player"+obj.player);
+  const $progressbar = createElement("div", "progressbar");
+  const $character = createElement("div", "character");
+  const $life = createElement("div", "life");
   $life.style.width = obj.hp + "%";
-  const $name = document.createElement("div");
-  $name.classList.add("name");
+  const $name = createElement("div", "name");
   $name.innerText = obj.name;
-  const $image = document.createElement("img");
+  const $image = createElement("img");
   $image.src = obj.img;
 
   $progressbar.appendChild($life);
@@ -78,10 +87,8 @@ function createPlayerStar(player, obj) {
   $player.appendChild($progressbar);
   $character.appendChild($image);
   $player.appendChild($character);
-
-  const $arena = document.querySelector(".arenas");
-  $arena.appendChild($player);
+  return $player;
 }
 
-//createPlayerStar("player1", player1);
-//createPlayerStar("player2", player2);
+$arena.appendChild(createPlayer(player1));
+$arena.appendChild(createPlayer(player2));
